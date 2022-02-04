975be4629fecf41bad8e36a9db159643bef10687
aditya-pethe <adityapethe@Adityas-MacBook-Pro-2.local>
2022-02-03 20:40:13 -0600

    initial commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+### Friktion Treasury Management
+
+Building out a data analytics and portfolio management framework for DAO Treasuries (Decentralized Autonomous Organizations) with Friktion Labs’ Research team
---
+++ sonar_scrape.py
@@ -0,0 +1,5 @@
+dao_addresses = {
+    'invictus':'HTYYDuePjjsm2dYwzWpYwkreMwS3KCigp3oZXhdw6fWn',
+    'saber':'Saber2gLauYim4Mvftnrasomsv6NvAuncvMEZwcLpD1',
+    'mango':'MangoCzJ36AjZyKwVj3VnYU4GTonjfVEnJmvvWaxLac'
+}
